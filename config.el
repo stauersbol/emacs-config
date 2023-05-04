d230ce241f113fe913f2677c3f7d7fef7e490923
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2023-05-04 15:37:53 +0200

    chore(config): disable blamer

--- config.el
+++ config.el
@@ -25,24 +25,24 @@
 (setq org-directory "~/org/")
 
 ;; BLAMER CONFIG
-(use-package blamer
-  :bind (("s-i" . blamer-show-commit-info))
-  :defer 20
-  :custom
-  (blamer-idle-time 0.3)
-  (blamer-min-offset 70)
-  :custom-face
-  (blamer-face ((t :foreground "#7a88cf"
-                   :background nil
-                   :height 110
-                   :italic t)))
-  :config
-  (global-blamer-mode t)
-  )
-(setq blamer-author-formatter " ✎ %s ")
-(setq blamer-datetime-formatter "[%s]")
-(setq blamer-commit-formatter " ● %s")
-(setq blamer-prettify-time-p t)
+;; (use-package blamer
+;;   :bind (("s-i" . blamer-show-commit-info))
+;;   :defer 20
+;;   :custom
+;;   (blamer-idle-time 0.3)
+;;   (blamer-min-offset 70)
+;;   :custom-face
+;;   (blamer-face ((t :foreground "#7a88cf"
+;;                    :background nil
+;;                    :height 110
+;;                    :italic t)))
+;;   :config
+;;   (global-blamer-mode t)
+;;   )
+;; (setq blamer-author-formatter " ✎ %s ")
+;; (setq blamer-datetime-formatter "[%s]")
+;; (setq blamer-commit-formatter " ● %s")
+;; (setq blamer-prettify-time-p t)
 
 ;; LSP CONFIG
 (use-package lsp-ui)
