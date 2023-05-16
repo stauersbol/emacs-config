30618f9f3f4e7e3877e645d6092661f47393cae6
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2023-05-16 20:02:48 +0200

    feat(init): add tree sitter to json

--- init.el
+++ init.el
@@ -140,7 +140,7 @@
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       (json +lsp)              ; At least it ain't XML
+       (json +ls +tree-sitter)              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
